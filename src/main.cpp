ccd197a94f3d7cf9ba394d2b4418248f8c808138
OswalB <grafitexpasto@gmail.com>
2026-01-15 20:34:27 -0500

    Test display + encoder sin modularizar

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,56 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <RotaryEncoder.h>
+#include <Bounce2.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+#define LED_PIN 13
+
+#define ENC_A 8   // CLK
+#define ENC_B 9   // DT
+#define ENC_SW 10 // Botón
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+RotaryEncoder encoder(ENC_A, ENC_B);
+Bounce button = Bounce();
+long lastPos = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+   Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+
+  lcd.init();        // Inicializa LCD
+  lcd.backlight();  // Enciende luz de fondo
+
+  lcd.setCursor(0, 0);
+  lcd.print("Proyecto Arduino");
+  lcd.setCursor(0, 1);
+  lcd.print("LCD 20x4 I2C");
+
+   pinMode(ENC_SW, INPUT_PULLUP);
+  button.attach(ENC_SW);
+  button.interval(5);
+
+  Serial.println("Encoder test start");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+encoder.tick();
+  button.update();
+
+  long newPos = encoder.getPosition();
+  if (newPos != lastPos) {
+    Serial.print("Position: ");
+    Serial.println(newPos);
+    lastPos = newPos;
+  }
+
+  if (button.fell()) {
+    Serial.println("Button pressed");
+  }
+
+  
 }
